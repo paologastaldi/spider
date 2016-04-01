462cad0ebc25c92338dd26faebc9a57ea93fb0c3
paologastaldi <paolo97.g@gmail.com>
2016-04-02 00:34:05 +0200

    aggiornamento db

--- dbspider.sql
+++ dbspider.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Creato il: Mar 19, 2016 alle 00:39
+-- Creato il: Apr 02, 2016 alle 00:32
 -- Versione del server: 10.1.10-MariaDB
 -- Versione PHP: 7.0.4
 
@@ -41,9 +41,22 @@ INSERT INTO `indirizzi` (`idUrl`, `dominio`, `urlPartenza`) VALUES
 (2, 'istruzione.it', 'http://www.istruzione.it/'),
 (3, 'lastampa.it', 'http://www.lastampa.it/'),
 (4, 'repubblica.it', 'http://www.repubblica.it/'),
-(5, 'www.corriere.it', 'http://www.corriere.it/'),
+(5, 'corriere.it', 'http://www.corriere.it/'),
 (6, 'studenti.it', 'http://www.studenti.it/'),
-(7, 'abctribe.com', 'http://it-it.abctribe.com/');
+(7, 'abctribe.com', 'http://it-it.abctribe.com/'),
+(8, 'wikipedia.org', 'https://it.wikipedia.org'),
+(9, 'tomshw.it', 'http://www.tomshw.it/'),
+(10, 'paginebianche.it', 'http://www.paginebianche.it'),
+(11, 'vallauri.edu', 'http://www.vallauri.edu'),
+(12, 'facebook.com', 'https://www.facebook.com'),
+(13, 'linkedin.com', 'https://it.linkedin.com'),
+(14, 'twitter.com', 'https://twitter.com'),
+(15, 'youtube.com', 'https://www.youtube.com'),
+(16, 'github.com', 'https://github.com/'),
+(17, 'polito.it', 'http://www.polito.it/'),
+(18, 'unito.it', 'http://www.unito.it/'),
+(19, 'paginegialle.it', 'http://www.paginegialle.it/'),
+(20, 'amazon.it', 'http://www.amazon.it/');
 
 -- --------------------------------------------------------
 
@@ -63,21 +76,21 @@ CREATE TABLE `professori` (
 --
 
 INSERT INTO `professori` (`idProf`, `cognome`, `contatore`, `selezionato`) VALUES
-(4, 'Rosso Maurizio', 0, 0),
-(5, 'Diego Nova', 0, 0),
-(6, 'Giraudo Giuseppe', 0, 0),
-(7, 'Tosello Giovanni', 0, 0),
+(4, 'Rosso Maurizio', 0, 1),
+(5, 'Diego Nova', 0, 1),
+(6, 'Giraudo Giuseppe', 0, 1),
+(7, 'Tosello Giovanni', 0, 1),
 (8, 'Pasquale Alessandra', 0, 1),
-(9, 'Tonello Sabrina', 0, 0),
-(10, 'Basiletti Emanuela', 0, 0),
+(9, 'Tonello Sabrina', 0, 1),
+(10, 'Basiletti Emanuela', 0, 1),
 (11, 'Puppo Marco', 0, 1),
-(12, 'Rosa Guido', 0, 0),
+(12, 'Rosa Guido', 0, 1),
 (13, 'Mina Guido', 0, 1),
-(14, 'Girardi Stefano', 0, 0),
-(15, 'Sperotto Sergio', 0, 0),
-(16, 'Ricerca', 0, 0),
+(14, 'Girardi Stefano', 0, 1),
+(15, 'Sperotto Sergio', 0, 1),
+(16, 'Ricerca', 0, 1),
 (17, 'ITIS', 0, 1),
-(18, 'Delpozzo', 0, 0);
+(18, 'Delpozzo', 0, 1);
 
 --
 -- Indici per le tabelle scaricate
@@ -103,7 +116,7 @@ ALTER TABLE `professori`
 -- AUTO_INCREMENT per la tabella `indirizzi`
 --
 ALTER TABLE `indirizzi`
-  MODIFY `idUrl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idUrl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT per la tabella `professori`
 --
